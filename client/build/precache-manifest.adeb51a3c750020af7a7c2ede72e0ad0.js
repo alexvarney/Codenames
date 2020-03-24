d8cb67e07059c02c43ce19495bc29f67ef6884b1
Alexander Varney <alex@varney.me>
2020-03-24 19:10:59 -0400

    add build to repo

---
+++ client/build/precache-manifest.adeb51a3c750020af7a7c2ede72e0ad0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0348ab88165608404b50e5f551124ecb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6cafba0b9df33982929",
+    "url": "/static/css/main.be4f4134.chunk.css"
+  },
+  {
+    "revision": "01b487278f333919f5d7",
+    "url": "/static/js/2.563513b6.chunk.js"
+  },
+  {
+    "revision": "d0ed0e93fdba385200229b33bb0108ea",
+    "url": "/static/js/2.563513b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6cafba0b9df33982929",
+    "url": "/static/js/main.02c8a0be.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  }
+]);
